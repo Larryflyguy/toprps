c8b080b617addd93b54fbf416239789f531916b6
Larryflyguy <71833480+Larryflyguy@users.noreply.github.com>
2022-03-13 18:41:56 -0400

    Add files via upload

--- rps.js
+++ rps.js
@@ -8,13 +8,35 @@ function game() {
 
 function playRound() {
     const playerSelection =
+    const computerSelection = computerChoice();
+
 }
 
 function playerChoice() {
+    let input = prompt("Type Rock, Paper, or Scissors");
+    while (input == null) {
+        input = prompt("Type Rock, Paper, or Scissors");
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input)
+    if (check == true) {
+        console.log(input);
+    }
 
+
+    console.log(input);
 }
 
 function computerChoice() {
     return choices[Math.floor(Math.random()*choices.length)]
 
+}
+
+function validateInput(choice) {
+    if(choices.includes(choice)) {
+        return true;
+    }   else {
+        return false;
+        }
+    }
 }
