8f12082e4138d556594c676b6e6a75bd766ad0c3
Larryflyguy <71833480+Larryflyguy@users.noreply.github.com>
2022-03-13 18:31:51 -0400

    Add files via upload

---
+++ rps.js
@@ -0,0 +1,20 @@
+const choices = ["rock", "papaer", "scissors"]
+
+function game() {
+    //play the game
+    //play five rounds
+    //console based
+}
+
+function playRound() {
+    const playerSelection =
+}
+
+function playerChoice() {
+
+}
+
+function computerChoice() {
+    return choices[Math.floor(Math.random()*choices.length)]
+
+}
